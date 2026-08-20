14148af79a7a167bf694d12f95d7e452e065aeb5
Sammi Chong <133241942+SammiChong@users.noreply.github.com>
2026-08-20 16:50:05 +0100

    PAY-8834 - Update to Infrastructure@2.5.1(perftest)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,7 +34,7 @@ module "ccpay-refunds-database-v15" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=DTSPO-30107-additional-postgres-admins"
   product              = var.product
   component            = var.component
   business_area        = "cft"
